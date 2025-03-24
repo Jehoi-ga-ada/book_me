437cb25c3e8a372796da796dbc1b3ca5b04b0e2b
Jehoi-ga-ada <sayajehoiada@gmail.com>
2025-03-24 16:13:21 +0700

    Assets and MapView

---
+++ BookMe/BookFormView.swift
@@ -0,0 +1,18 @@
+//
+//  BookFormView.swift
+//  BookMe
+//
+//  Created by Jehoiada Wong on 24/03/25.
+//
+
+import SwiftUI
+
+struct BookFormView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BookFormView()
+}
--- BookMe/ContentView.swift
+++ BookMe/ContentView.swift
@@ -1,88 +1,104 @@
-//
-//  ContentView.swift
-//  BookMe
-//
-//  Created by Jehoiada Wong on 17/03/25.
-//
-
 import SwiftUI
-import CoreData
+
+let backgroundGradient = LinearGradient(
+    colors: [Color("top_gradient"), Color("bottom_gradient")],
+    startPoint: .top,
+    endPoint: .bottom
+)
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
+    @State private var offset: CGSize = .zero
+    @State private var lastOffset: CGSize = .zero
 
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
+    @State private var mapScale: CGFloat = 1.5
+    @GestureState private var magnifyBy: CGFloat = 1.0
+    
+    let minimumZoomScale: CGFloat = 1.0
+    let maximumZoomScale: CGFloat = 3.0
+    
+    // MARK: - Magnification Gesture Logic
+    var magnification: some Gesture {
+        MagnifyGesture()
+            .updating($magnifyBy){ value, gestureState, transaction in
+                gestureState = value.magnification
+            }.onEnded { value in
+                mapScale *= value.magnification
+            }
+    }
 
+    // MARK: - Drag Gesture Logic
+    var drag: some Gesture {
+        DragGesture()
+            .onChanged { gesture in
+                offset = CGSize(
+                    width: lastOffset.width + gesture.translation.width,
+                    height: lastOffset.height + gesture.translation.height
+                )
+            }
+            .onEnded { _ in
+                lastOffset = offset
+            }
+    }
+    
+    // MARK: - Pin Points Location
+    var pinPointsLocation: [CGPoint] {
+        [
+            CGPoint(x: 490, y: 120),
+            CGPoint(x: 490, y: 50),
+            CGPoint(x: 490, y: 0),
+            CGPoint(x: -60, y: 90),
+            CGPoint(x: -160, y: 90),
+            CGPoint(x: -220, y: 70),
+            CGPoint(x: -285, y: -220),
+            CGPoint(x: -395, y: -200),
+            CGPoint(x: -470, y: -90),
+            CGPoint(x: -445, y: -170),
+        ]
+    }
+    
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+        ZStack {
+            // MARK: - Background
+            backgroundGradient
+            Rectangle()
+                .fill(
+                    ImagePaint(
+                        image: Image("map_background"),
+                        scale: 0.3
+                    )
+                )
+                .opacity(0.65)
+                .edgesIgnoringSafeArea(.all)
+            // MARK: - Map Content
+            VStack {
+                ZStack{
+                    Image("academy_map")
+                        .resizable()
+                        .scaledToFit()
+                        .scaleEffect(mapScale*magnifyBy)
+                    ForEach(pinPointsLocation.indices, id: \.self) { index in
+                        let pos = pinPointsLocation[index]
+                        Button(action: {
+                            print("pin point pressed!. \(index)")
+                        })
+                        {
+                            Image("pin_point")
+                                .resizable()
+                                .scaledToFit()
+                        }
+                        .position(pos)
+                        .scaleEffect(mapScale/3*magnifyBy)
+                        .frame(width: 36, height: 40)
                     }
+                    
                 }
-                .onDelete(perform: deleteItems)
+                .offset(offset)
             }
-            .toolbar {
-#if os(iOS)
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-#endif
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
+        }.gesture(drag.simultaneously(with: magnification))
+        .ignoresSafeArea()
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
 #Preview {
-    ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    ContentView()
 }
