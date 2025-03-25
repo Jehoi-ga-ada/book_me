6ff2206895e8a41dd8570884461b83b5ea749aac
Jehoi-ga-ada <sayajehoiada@gmail.com>
2025-03-25 18:52:42 +0700

    FormView Page

--- BookMe/BookFormView.swift
+++ BookMe/BookFormView.swift
@@ -8,8 +8,42 @@
 import SwiftUI
 
 struct BookFormView: View {
+    var colab_name: String = "Collab Room 1"
+    @State private var date = Date()
+    @State private var name: String = "Budiono"
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            HStack{
+                Text(colab_name)
+                DatePicker(
+                    "Start Date",
+                    selection: $date,
+                    displayedComponents: [.date]
+                )
+            }
+            HStack{
+                Image(systemName: "square.and.arrow.up")
+                Image(systemName: "square.and.arrow.up")
+                Image(systemName: "square.and.arrow.up")
+                Spacer()
+            }
+            List {
+                // "Masukan Nama" row
+                NavigationLink(destination: ContentView()) {
+                    HStack {
+                        Text("Masukan Nama")
+                        Spacer()
+                        // Show the current name in a secondary color
+                        Text(name)
+                            .foregroundColor(.secondary)
+                    }
+                }
+            }.frame()
+            ScrollView{
+                
+            }
+        }
     }
 }
 
