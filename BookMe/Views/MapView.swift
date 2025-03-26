5117c5e52a90192b9375fb5e32880a86f3f0b061
Jehoi-ga-ada <sayajehoiada@gmail.com>
2025-03-26 14:54:33 +0700

    added CollabRoom class

--- BookMe/Views/MapView.swift
+++ BookMe/Views/MapView.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import Observation
 
 struct MapView: View {
     let backgroundGradient = LinearGradient(
@@ -6,6 +7,8 @@ struct MapView: View {
         startPoint: .top,
         endPoint: .bottom
     )
+    
+    @State private var allCollabRooms: [CollabRoomModel] = CollabRoomData.generateCollabRooms()
     
     @State private var offset: CGSize = .zero
     @State private var lastOffset: CGSize = .zero
@@ -57,29 +60,6 @@ struct MapView: View {
             }
     }
     
-    var pinPointsLocation: [CGPoint] {
-        [
-            CGPoint(x: 490, y: 120), // Collab 7
-            CGPoint(x: 490, y: 0), // Collab 7A
-            CGPoint(x: -60, y: 90), // Collab 1
-            CGPoint(x: -160, y: 90), // Collab 2
-            CGPoint(x: -220, y: 70), // Collab 3
-            CGPoint(x: -395, y: -200), // Collab 5
-            CGPoint(x: -470, y: -90), // Collab 4
-        ]
-    }
-    
-    var pinPointsZoomLocation: [CGPoint] {
-        [
-            CGPoint(x: -464.6667, y: -405.0), //0
-            CGPoint(x: -463.6666, y: -270.0), //1
-            CGPoint(x: 78.6667, y: -379.0),   //2
-            CGPoint(x: 191.0, y: -359.3333),  //3
-            CGPoint(x: 230.3333, y: -330.3333), //4
-            CGPoint(x: 411.3333, y: -83.6667), //5
-            CGPoint(x: 460.0, y: -180.0),      //6
-        ]
-    }
     
     private func focusOnPin(_ pin: CGPoint) {
         withAnimation {
@@ -111,18 +91,16 @@ struct MapView: View {
                         .scaledToFit()
                         .scaleEffect(mapScale * magnifyBy)
                     
-                    ForEach(pinPointsLocation.indices, id: \.self) { index in
-                        let pos = pinPointsLocation[index]
-                        let zoom = pinPointsZoomLocation[index]
+                    ForEach(allCollabRooms, id: \.name) { collabRoom in
                         Button(action: {
-                            focusOnPin(zoom)
-                            print("pin point pressed! \(index)")
+                            focusOnPin(collabRoom.pinPointsZoomLocation)
+                            print("pin point pressed! \(collabRoom.name)")
                         }) {
                             Image("pin_point")
                                 .resizable()
                                 .scaledToFit()
                         }
-                        .position(pos)
+                        .position(collabRoom.pinPointsLocation)
                         .scaleEffect(mapScale / 3 * magnifyBy)
                         .frame(width: 36, height: 40)
                     }
@@ -155,4 +133,74 @@ struct MapView: View {
 
 #Preview {
     MapView()
+}
+
+struct CollabRoomModel {
+    var name: String
+    var pinPointsLocation: CGPoint
+    var pinPointsZoomLocation: CGPoint
+    var capacity: Int
+}
+
+@Observable
+final class CollabRoomData {
+    static func generateCollabRooms() -> [CollabRoomModel] {
+        var pinPointsLocation: [CGPoint] {
+            [
+                CGPoint(x: 490, y: 120), // Collab 7
+                CGPoint(x: 490, y: 0), // Collab 7A
+                CGPoint(x: -60, y: 90), // Collab 1
+                CGPoint(x: -160, y: 90), // Collab 2
+                CGPoint(x: -220, y: 70), // Collab 3
+                CGPoint(x: -395, y: -200), // Collab 5
+                CGPoint(x: -470, y: -90), // Collab 4
+            ]
+        }
+        
+        var pinPointsZoomLocation: [CGPoint] {
+            [
+                CGPoint(x: -464.6667, y: -405.0), //0
+                CGPoint(x: -463.6666, y: -270.0), //1
+                CGPoint(x: 78.6667, y: -379.0),   //2
+                CGPoint(x: 191.0, y: -359.3333),  //3
+                CGPoint(x: 230.3333, y: -330.3333), //4
+                CGPoint(x: 411.3333, y: -83.6667), //5
+                CGPoint(x: 460.0, y: -180.0),      //6
+            ]
+        }
+        
+        var names: [String] {
+            [
+                "Collab 7",
+                "Collab 7A",
+                "Collab 1",
+                "Collab 2",
+                "Collab 3",
+                "Collab 5",
+                "Collab 4",
+            ]
+        }
+        
+        var capacaties: [Int] {
+            [
+                6,
+                8,
+                8,
+                8,
+                8,
+                8,
+                8,
+                8
+            ]
+        }
+        
+        var collabRooms: [CollabRoomModel] = []
+        for i in 0..<names.count {
+            collabRooms.append(CollabRoomModel(name: names[i],
+                                          pinPointsLocation: pinPointsLocation[i],
+                                          pinPointsZoomLocation: pinPointsZoomLocation[i],
+                                          capacity: capacaties[i]))
+        }
+        return collabRooms
+    }
 }
